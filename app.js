54518c80c69eaed72cfce8aed617bde50fb55441
Christian <chnikel@gmail.com>
2020-02-03 14:00:22 +0100

    Initial Commit

--- app.js
+++ app.js
@@ -4,18 +4,6 @@ const modalContainer = document.getElementById('modal-container');
 const navigationElement = document.getElementById('nav');
 
 document.addEventListener('DOMContentLoaded', event => {
-  const navLinks = navigationElement.querySelectorAll('.navigation-link');
-  console.log(navLinks);
-  for (const nav of navLinks) {
-    nav.addEventListener('click', event => {
-      for (const nav of navLinks) {
-        if (nav.classList.contains('active')) {
-          nav.classList.remove('active');
-        }
-        event.target.classList.add('active');
-      }
-    });
-  }
   modalButton.addEventListener('click', event => {
     modalContainer.classList.add('fade-up');
     body.classList.remove('modal-open');
