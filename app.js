594cd0155755e330b78dcf4159a9112ac6fefcd9
Christian <chnikel@gmail.com>
2020-02-04 16:20:43 +0100

    Sectionen angefangen

--- app.js
+++ app.js
@@ -1,37 +1,47 @@
-const body = document.querySelector('.modal-open');
 const modalButton = document.getElementById('modal__button');
 const modalContainer = document.getElementById('modal-container');
 const navigationElement = document.getElementById('nav');
 
 document.addEventListener('DOMContentLoaded', event => {
-  console.log(modalButton);
-
-  modalButton.addEventListener('click', event => {
-    modalContainer.classList.add('fade-up');
-    body.classList.remove('modal-open');
-    setTimeout(() => {
-      //modalContainer.style.display = 'none';
-    }, 1000);
-  });
+  if (modalButton) {
+    modalButton.addEventListener('click', event => {
+      modalContainer.classList.add('fade-up');
+      setTimeout(() => {
+        //modalContainer.style.display = 'none';
+      }, 1000);
+    });
+  }
 
   const menuOpen = document.getElementById('topbar__menu__open');
   const menuClose = document.getElementById('topbar__menu__close');
   const navigation = document.getElementById('navigaion');
   const topbar = document.getElementById('topbar');
+  const navLinks = document.querySelectorAll('.navigaion__link');
+
+  const showNavigation = () => {
+    navigation.classList.add('active');
+    topbar.style.opacity = 1;
+  };
+  const hideNavigation = () => {
+    navigation.classList.remove('active');
+    topbar.style.opacity = 0.85;
+  };
 
   menuOpen.addEventListener('click', event => {
     menuOpen.style.display = 'none';
     menuClose.style.display = 'flex';
     //show Navigation
-    navigation.classList.add('active');
-    topbar.style.opacity = 1;
+    showNavigation();
   });
 
   menuClose.addEventListener('click', event => {
     menuOpen.style.display = 'flex';
     menuClose.style.display = 'none';
     //hide Navigation
-    navigation.classList.remove('active');
-    topbar.style.opacity = 0.85;
+    hideNavigation();
   });
+
+  for (const link of navLinks) {
+    link.addEventListener('click', event => {});
+  }
 });
