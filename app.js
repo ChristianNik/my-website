ec9511bd5d5450cedcf68a300437aeb08f0b7dc1
Christian <chnikel@gmail.com>
2020-02-16 20:12:00 +0100

    maueller merge (navigation v2) - 1

--- app.js
+++ app.js
@@ -3,107 +3,98 @@ const modalCloseButtons = document.querySelectorAll('.modal__close');
 const navigationElement = document.getElementById('nav');
 
 document.addEventListener('DOMContentLoaded', event => {
-  //
-  // Modal
-  //
-  if (modalCloseButtons) {
-    for (const button of modalCloseButtons) {
-      button.addEventListener('click', event => {
-        button.parentElement.classList.add('fade-up');
-        setTimeout(() => {
-          button.parentElement.classList.remove('active');
-        }, 1000);
-      });
-    }
-  }
+	//
+	// Modal
+	//
+	if (modalCloseButtons) {
+		for (const button of modalCloseButtons) {
+			button.addEventListener('click', event => {
+				button.parentElement.classList.add('fade-up');
+				setTimeout(() => {
+					button.parentElement.classList.remove('active');
+				}, 1000);
+			});
+		}
+	}
 
-  //
-  // Navigation
-  //
-  const body = document.getElementById('body');
-  const menuOpen = document.getElementById('topbar__menu__open');
-  const menuClose = document.getElementById('topbar__menu__close');
-  const navigation = document.getElementById('navigaion');
-  const topbar = document.getElementById('topbar');
-  const navLinksMobile = document.querySelectorAll('.navigaion__link');
-  const navLinksDesktop = document.querySelectorAll('.topbar__link');
+	//
+	// Navigation
+	//
+	const body = document.getElementById('body');
+	const menuOpen = document.getElementById('btn__topbar__open');
+	const menuClose = document.getElementById('btn__topbar__close');
+	const topbar = document.getElementById('topbar');
+	const navLinks = document.querySelectorAll('.nav__link');
 
-  const showNavigation = () => {
-    menuOpen.style.display = 'none';
-    menuClose.style.display = 'flex';
+	const showNavigation = () => {
+		body.style.overflow = 'hidden';
 
-    navigation.classList.remove('slideOutUp');
-    navigation.classList.add('slideInDown');
+		menuOpen.classList.remove('active');
+		menuClose.classList.add('active');
 
-    topbar.style.opacity = 1;
-    body.style.overflow = 'hidden';
-  };
-  const hideNavigation = () => {
-    menuOpen.style.display = 'flex';
-    menuClose.style.display = 'none';
+		topbar.classList.add('tb--extended');
+	};
 
-    navigation.classList.remove('slideInDown');
-    navigation.classList.add('slideOutUp');
+	const hideNavigation = () => {
+		body.style.overflow = '';
 
-    topbar.style.opacity = 0.85;
-    body.style.overflow = '';
-  };
+		menuOpen.classList.add('active');
+		menuClose.classList.remove('active');
 
-  const changeActive = aktiveLink => {
+		topbar.classList.remove('tb--extended');
+	};
+
+	/*
+  const changeActiveNavigation = aktiveLink => {
     navLinksDesktop.forEach(value => {
       value === aktiveLink
         ? value.classList.add('active')
         : value.classList.remove('active');
     });
   };
+  */
 
-  menuOpen.addEventListener('click', event => {
-    showNavigation();
-  });
+	menuOpen.addEventListener('click', event => {
+		showNavigation();
+	});
 
-  menuClose.addEventListener('click', event => {
-    hideNavigation();
-  });
+	menuClose.addEventListener('click', event => {
+		hideNavigation();
+	});
 
-  for (const link of navLinksMobile) {
-    link.addEventListener('click', event => {
-      hideNavigation();
-    });
-  }
+	for (const navLink of navLinks) {
+		navLink.addEventListener('click', event => {
+			hideNavigation();
+		});
+	}
 
-  for (const link of navLinksDesktop) {
-    link.addEventListener('click', event => {
-      changeActive(event.target);
-    });
-  }
+	//
+	// Image click
+	//
+	const projectImage = document.querySelectorAll('.item__image');
 
-  //
-  // Image click
-  //
-  const projectImage = document.querySelectorAll('.item__image');
+	if (projectImage) {
+		for (const image of projectImage) {
+			image.addEventListener('click', () => {
+				alert('Diese Funktion ist noch nicht verfügbar.');
+			});
+		}
+	}
 
-  if (projectImage) {
-    for (const image of projectImage) {
-      image.addEventListener('click', () => {
-        alert('Diese Funktion ist noch nicht verfügbar.');
-      });
-    }
-  }
+	//
+	// To Top Button
+	//
+	const toTopButton = document.getElementById('toTop');
 
-  //
-  // To Top Button
-  //
-  const toTopButton = document.getElementById('toTop');
-
-  // When the user scrolls down 20px from the top of the document, show the button
-  document.addEventListener('scroll', () => {
-    if (
-      document.body.scrollTop > 20 ||
-      document.documentElement.scrollTop > 20
-    ) {
-      toTopButton.style.display = 'block';
-    } else {
-      toTopButton.style.display = 'none';
-    }
-  });
+	// When the user scrolls down 20px from the top of the document, show the button
+	document.addEventListener('scroll', () => {
+		if (
+			document.body.scrollTop > 20 ||
+			document.documentElement.scrollTop > 20
+		) {
+			toTopButton.style.display = 'block';
+		} else {
+			toTopButton.style.display = 'none';
+		}
+	});
 });
